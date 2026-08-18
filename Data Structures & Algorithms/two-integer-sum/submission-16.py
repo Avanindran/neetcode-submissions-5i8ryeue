bdf604465cbd5fa269355235da5b302cb821d22d
Avanindran <140926051+Avanindran@users.noreply.github.com>
2026-08-18 10:44:48 +0800

    Add: two-integer-sum - submission-16

---
+++ Data Structures & Algorithms/two-integer-sum/submission-16.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        #hashmap solution
+
+        seen = {}
+
+        for i,num in enumerate(nums):
+            comp = target - num
+
+            if comp in seen:
+                return [seen[comp], i]
+            
+            else:
+                seen[num] = i
+            
+        
+
+        
